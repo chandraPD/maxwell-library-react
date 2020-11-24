4f62eafcddb7798b4a08733ccf886706a7f0e229
jskyte <jayson199732@gmail.com>
2020-11-24 16:28:32 +0700

    added auth page

--- src/Navigations/MainNavigations.js
+++ src/Navigations/MainNavigations.js
@@ -4,11 +4,16 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Footer from '../Screens/Footer/Footer'
 import Navbar from '../Screens/Navbar/Navbar'
 import Sidebar from '../Screens/SideBar/Sidebar'
+import Auth from '../Screens/Auth/Auth'
 
 class MainNavigation extends Component {
   render() {
     return(
       <Router>
+        <Route path='/auth'>
+          <Auth />
+        </Route>
+        
         <Route path='/'>
           <Navbar />
           <Sidebar />
