334e8045f766bfb74cc0461e8b703c436a6913e3
Kevin <kevinsuharto98@gmail.com>
2020-11-24 17:22:40 +0800

    Detail

--- src/Navigations/MainNavigations.js
+++ src/Navigations/MainNavigations.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Footer from '../Screens/Footer/Footer'
 import Navbar from '../Screens/NavBar/NavBar'
 import Sidebar from '../Screens/SideBar/SideBar'
-import Detail from '../Screens/Contents/Detail'
+import Detail from '../Screens/Contents/Detail/Detail'
 
 class MainNavigation extends Component {
   render() {
