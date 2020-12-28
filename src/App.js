c2814394f08ca38e86ca475ebf9df159e73d9f37
chandraPD <chandraputradinata@gmail.com>
2020-12-28 13:00:40 +0700

    setting front end base_url

--- src/App.js
+++ src/App.js
@@ -1,22 +1,5 @@
 import MainNavigation from './Navigations/MainNavigations'
 
-// css
-import './Assets/vendors/plugins/fontawesome-free/css/all.min.css'
-import './Assets/vendors/plugins/overlayScrollbars/css/OverlayScrollbars.min.css'
-import './Assets/vendors/dist/css/adminlte.min.css'
-import './Assets/vendors/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css'
-import './Assets/vendors/plugins/daterangepicker/daterangepicker.css'
-import './Assets/vendors/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css'
-import './Assets/vendors/plugins/datatables-responsive/css/responsive.bootstrap4.min.css'
-
-// js
-import './Assets/vendors/plugins/jquery/jquery.min.js'
-import './Assets/vendors/plugins/bootstrap/js/bootstrap.bundle.min.js'
-import './Assets/vendors/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js'
-// import './Assets/vendors/dist/js/adminlte.min.js'
-import './Assets/vendors/plugins/datatables/jquery.dataTables.min.js'
-import './Assets/vendors/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js'
-import './Assets/vendors/plugins/bootstrap-switch/js/bootstrap-switch.min.js'
 
 function App() {
   return (    
