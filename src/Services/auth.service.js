5875fffd537c0fa37a449e4a1a2de4a05e15a75f
mdayann <mdayan312@gmail.com>
2021-01-07 09:05:18 +0700

    change API endpoint

--- src/Services/auth.service.js
+++ src/Services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://maxwell-backend.herokuapp.com/auth/";
+const API_URL = "http://localhost:8080/auth/";
 
 class AuthService {
   login(email, password) {
