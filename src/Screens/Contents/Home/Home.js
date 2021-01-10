df3576e4fed5ad6ab61d0b90e419649f86695b5c
Mochamad Farid Naufal <mochamad.farid.n@outlook.com>
2021-01-10 09:50:39 +0700

    commit lagi

--- src/Screens/Contents/Home/Home.js
+++ src/Screens/Contents/Home/Home.js
@@ -93,7 +93,6 @@ class Home extends Component {
                         </div>
                     )
                   })}
-
               {/* <div
                 className="carousel-cell"
                 style={{
