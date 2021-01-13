4b80093f851ff3e8f94cdb16a98ad68c90d44ab4
jskyte <jayson199732@gmail.com>
2021-01-13 17:32:48 +0700

    minor fix and new validation for category and book

--- src/Screens/NavBar/NavBar.js
+++ src/Screens/NavBar/NavBar.js
@@ -27,30 +27,7 @@ class NavBar extends Component {
             {/* Left navbar links */}
             <ul className="navbar-nav">
                 <li className="nav-item dropdown">
-                <a id="dropdownSubMenu1" role="button" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" className="nav-link dropdown-toggle">All Categories</a>
-                <ul aria-labelledby="dropdownSubMenu1" className="dropdown-menu border-0 shadow" style={{left: 0, right: 'inherit'}}>
-                    <li><a href="./category.html" className="dropdown-item">Novel</a></li>
-                    <li><a href="./category.html" className="dropdown-item">Comic Book</a></li>
-                    <li>
-                    <a href="./category.html" className="dropdown-item">Detective and Mystery</a>
-                    </li>
-                    <li><a href="./category.html" className="dropdown-item">Historical Fiction</a></li>
-                    <li><a href="./category.html" className="dropdown-item">Horror</a></li>
-                    <li><a href="./category.html" className="dropdown-item">literary Fiction</a></li>
-                    {/* End Level two */}
-                </ul>
-                </li>
-                <li className="nav-item dropdown">
-                <a id="dropdownSubMenu1" role="button" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" className="nav-link dropdown-toggle">All Time</a>
-                <ul aria-labelledby="dropdownSubMenu1" className="dropdown-menu border-0 shadow" style={{left: 0, right: 'inherit'}}>
-                    <li><a href="./year.html" className="dropdown-item">2020</a></li>
-                    <li><a href="./year.html" className="dropdown-item">2019</a></li>
-                    <li><a href="./year.html" className="dropdown-item">2018</a></li>
-                    <li><a href="./year.html" className="dropdown-item">2017</a></li>
-                    <li><a href="./year.html" className="dropdown-item">2016</a></li>
-                    <li><a href="./year.html" className="dropdown-item">2015</a></li>
-                    {/* End Level two */}
-                </ul>
+                <Link to='/Catalogue' role="button" aria-haspopup="true" aria-expanded="false" className="nav-link">All Books</Link>
                 </li>
             </ul>
             {/* SEARCH FORM */}
