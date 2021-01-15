836832ce614b48621571bd12b8ced45cc319de05
Kevin <kevinsuharto98@gmail.com>
2021-01-15 08:58:56 +0700

    test

--- src/Instances/axios-instances.js
+++ src/Instances/axios-instances.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 console.log(localStorage)
 const instance = axios.create({
-    baseURL: 'http://localhost:8080/',
-    // baseURL: 'https://maxwell-library.herokuapp.com/',
+    // baseURL: 'http://localhost:8080/',
+    baseURL: 'https://maxwell-library.herokuapp.com/',
   });
 
   if(localStorage.getItem('user')  !== null){
